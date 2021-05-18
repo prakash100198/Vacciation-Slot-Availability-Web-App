24610132b59494962d9c6eff3787a694b0586479
Prakash Kumar <prakashkumar100198@gmail.com>
2021-05-18 23:13:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,11 @@
-# Modified from
-# https://github.com/MaartenGr/streamlit_guide/blob/master/Procfile
-# and
-# https://github.com/MaartenGr/streamlit_guide/blob/master/setup.sh
-
-# mkdir -p ~/.streamlit/
-
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS=false\n\
-# port = $PORT\n\
-# " > ~/.streamlit/config.toml
-
 mkdir -p ~/.streamlit/
-# echo "\
-# [general]\n\
-# email = \"your-email@domain.com\"\n\
-# "> ~/.streamlit/credentials.toml
+echo "\
+[general]\n\
+email = \"prakashkumar100198@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
