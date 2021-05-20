0fccdce8c556f6fcdc0ce98dea421b0bab91fb77
Prakash Kumar <prakashkumar100198@gmail.com>
2021-05-20 14:04:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -336,13 +336,13 @@ else:
 st.write('\n\n\n')
 st.text('''Important Note:\n
 By using the web-app, you agree and understand
-that any information provided in the
-website on an as-is basis may change 
-anytime without any notification or intimation. 
-You also understand that the resources being 
-provided are crowdsourced and TPH or any of 
-its members do not take responsibility for the 
-truth of any information provided in the website.
-People using the platform are advised to use caution
-and do their own research and due diligence before 
+that any information provided in the website 
+on an as-is basis may change anytime without 
+any notification or intimation. You also 
+understand that the resources being provided
+are crowdsourced and TPH or any of its members
+do not take responsibility for the truth of any 
+information provided in the website.People using 
+the platform are advised to use caution and do
+their own research and due diligence before 
 using the resources provided.''')
